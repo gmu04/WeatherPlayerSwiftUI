ae9b12d0b796c6fb83019b76edbe38332f488eab
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-10-26 14:34:30 +0300

    Initial Commit

---
+++ PlayVideoSwiftUI/PlayVideoSwiftUIApp.swift
@@ -0,0 +1,12 @@
+// PlayVideoSwiftUIApp.swift by Gokhan Mutlu on 26.10.2024
+
+import SwiftUI
+
+@main
+struct PlayVideoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
